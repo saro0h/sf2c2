fadf29a91f46e96b849b2002c6b27addd7d0c7bd
sarah khalil <mkhalil.sarah@gmail.com>
2015-10-15 13:33:15 +0200

    Added the features to play the hangman

--- src/AppBundle/Controller/GameController.php
+++ src/AppBundle/Controller/GameController.php
@@ -2,10 +2,17 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Game\GameRunner;
+use AppBundle\Game\WordList;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Game\GameContext;
+use AppBundle\Game\Loader\XmlFileLoader;
+use AppBundle\Game\Loader\TextFileLoader;
 
 /**
  * @Route(
@@ -20,7 +27,7 @@ class GameController extends Controller
      */
     public function homeAction()
     {
-        return $this->render('AppBundle:Game:home.html.twig');
+        return $this->render('AppBundle:Game:home.html.twig', array('game' => $this->getGameRunner()->loadGame()));
     }
 
     /**
@@ -29,7 +36,9 @@ class GameController extends Controller
      */
     public function wonAction()
     {
-        return array();
+        $game = $this->getGameRunner()->resetGameOnSuccess();
+
+        return array(array('game' => $game));
     }
 
     /**
@@ -37,7 +46,51 @@ class GameController extends Controller
      */
     public function failedAction()
     {
-        return new Response($this->renderView('AppBundle:Game:failed.html.twig'));
+        $game = $this->getGameRunner()->resetGameOnFailure();
+
+        return new Response($this->renderView('AppBundle:Game:failed.html.twig', array('game' => $game)));
+    }
+
+    /**
+     * @Route("/reset", name="game_reset")
+     */
+    public function resetAction()
+    {
+        $this->getGameRunner()->resetGame();
+
+        return $this->redirectToRoute('game_home');
+    }
+
+    /**
+     * This action plays a letter.
+     *
+     * @Route("/play/{letter}", name="game_play_letter", requirements={
+     *   "letter"="[A-Z]"
+     * })
+     * @Method("GET")
+     */
+    public function playLetterAction($letter)
+    {
+        $game = $this->getGameRunner()->playLetter($letter);
+
+        if (!$game->isOver()) {
+            return $this->redirectToRoute('game_home');
+        }
+
+        return $this->redirectToRoute($game->isWon() ? 'game_won' : 'game_failed');
+    }
+
+    /**
+     * This action plays a word.
+     *
+     * @Route("/play", name="game_play_word", condition="request.request.has('word')")
+     * @Method("POST")
+     */
+    public function playWordAction(Request $request)
+    {
+        $game = $this->getGameRunner()->playWord($request->request->get('word'));
+
+        return $this->redirectToRoute($game->isWon() ? 'game_won' : 'game_failed');
     }
 
     /**
@@ -50,5 +103,22 @@ class GameController extends Controller
             'Martin Durand' => 'Best web application ever',
             'Paul Smith' => 'Awesomeness!',
         ));
+    }
+
+    private function getGameRunner()
+    {
+        $wordlist = new WordList();
+
+        $wordlist->addLoader('txt', new TextFileLoader());
+        $wordlist->addLoader('xml', new XmlFileLoader());
+
+        $wordlist->loadDictionaries(array(
+            $this->getParameter('kernel.root_dir').'/Resources/data/words.txt',
+            $this->getParameter('kernel.root_dir').'/Resources/data/words.xml'
+        ));
+
+        $gameContext = new GameContext($this->get('session'));
+
+        return new GameRunner($gameContext, $wordlist);
     }
 }
